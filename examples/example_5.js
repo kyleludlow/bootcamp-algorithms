27e66a1ef33197084484e7645454369b9533bc1a
OoogleBoogle <simattfield@gmail.com>
2016-06-13 16:11:41 -0700

    Can't one-line it :(

--- examples/example_5.js
+++ examples/example_5.js
@@ -1,13 +1,20 @@
-function splitter(word, seperator, start = 0, end = 0, splits = []) {
-  if (end === word.length) {
-    splits.push(word.slice(start, end));
-    return splits
-  }
-  if (word.charAt(end) === seperator) {
-    splits.push(word.slice(start, end))
-    start = end + 1
-  }
-  return splitter(word, seperator, start, end + 1, splits)
+let splits = [];
+
+function blankCheck(word, start, end) {
+  let str = word.slice(start, end)
+  if (str) splits.push(str);
 }
 
-console.log(splitter("Split-Me--William", "-"));
+function splitter(word, seperator, start = 0, end = 0) {
+  if (end === word.length) {
+    blankCheck(word, start, end);
+    return splits;
+  }
+  if (word.charAt(end) === seperator) {
+    blankCheck(word, start, end);
+    start = end + 1
+  }
+  return splitter(word, seperator, start, end + 1)
+}
+
+console.log(splitter("Split-me---without-------empty--strings---", "-"));
