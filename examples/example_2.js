ae73ed6e78a26bdfed34ad35f6384e19c9789e7e
OoogleBoogle <simattfield@gmail.com>
2016-06-13 14:53:07 -0700

    Refactored

--- examples/example_2.js
+++ examples/example_2.js
@@ -1,11 +1,8 @@
 // 2. reverse a string
 
-function revStr(str, index = -1, newString = "") {
-  if (newString.length === str.length) {
-      return newString;
-  }
-  newString += str.slice(index).charAt(0);
-  return revStr(str, index - 1, newString);
+function revStr(str, index = 0, newString = "") {
+  newString += str.charAt(str.length - index);
+  return newString.length === str.length ? newString : revStr(str, index + 1, newString);
 }
 
 console.log(revStr('yvan eht nioj'));
