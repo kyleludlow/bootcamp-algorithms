b1da5877a6ba951d6c2951a60e4fae12b0947b40
OoogleBoogle <simattfield@gmail.com>
2016-06-13 15:47:01 -0700

    More 1 line refactors - it's really unnecessary...

--- examples/example_4.js
+++ examples/example_4.js
@@ -1,9 +1,12 @@
 // Exercise 4: Calculates the nth triangular number.
 
-function nth(number, result = 0, start = 0) {
-    if (number === start) return result;
-    start++;
-    result += start;
-    return nth(number, result, start);
+function nthTri(number, result = 0, start = 0) {
+  return number === 0 ? number : number === start ? result : nthTri(number, (result += start + 1), start + 1);
 }
-console.log(nth(3));
+
+console.log(nthTri(0));
+console.log(nthTri(1));
+console.log(nthTri(2));
+console.log(nthTri(3));
+console.log(nthTri(4));
+console.log(nthTri(5));
