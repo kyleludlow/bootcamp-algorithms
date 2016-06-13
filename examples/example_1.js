00a8176441f695b9a0b1dcfdc9c19b016f979ab8
OoogleBoogle <simattfield@gmail.com>
2016-06-13 15:23:01 -0700

    Going on a one line rampage

--- examples/example_1.js
+++ examples/example_1.js
@@ -1,27 +1,7 @@
 // 1. Work out whether a number is even
 
-var isEven = function(number){
-
-  if (number < 0){
-    number = number * -1;
-  }
-
-  if (number  === 0){
-    return true;
-  } else if (number === 1){
-    return false;
-  }
-
-return isEven(number  - 2);
+function isEven(number) {
+  return number < 0 ? isEven(-number) : number === 0 ? true : number === 1 ? false : isEven(number - 2);
 };
 
-
-// USE THE (brute)FORCE
-var isEven = function(number) {
-  if (number % 2 === 0){
-    return true
-  }
-  else {
-    return false
-  }
-};
+console.log(isEven(-4));
