39319917d62972ab2921278ed954dd430818776e
OoogleBoogle <simattfield@gmail.com>
2016-06-14 16:22:03 -0700

    Added error checking and multi char separator

--- examples/example_5.js
+++ examples/example_5.js
@@ -1,20 +1,23 @@
-let splits = [];
-
-function blankCheck(word, start, end) {
+function blankCheck(word, start, end, splits) {
   let str = word.slice(start, end)
   if (str) splits.push(str);
 }
 
-function splitter(word, seperator, start = 0, end = 0) {
-  if (end === word.length) {
-    blankCheck(word, start, end);
+function splitter(word, seperator, start = 0, end = 0, splits = []) {
+  if (!seperator) {
+    seperator = ' ';
+  }
+  if (end === word.length ) {
+    blankCheck(word, start, end, splits);
     return splits;
   }
-  if (word.charAt(end) === seperator) {
-    blankCheck(word, start, end);
-    start = end + 1
+  if (word.substr(end, seperator.length) === seperator) {
+    blankCheck(word, start, end, splits);
+    start = end + seperator.length
   }
-  return splitter(word, seperator, start, end + 1)
+  return splitter(word, seperator, start, end + 1, splits)
 }
 
-console.log(splitter("Split-me---without-------empty--strings---", "-"));
+console.log(splitter("Split---without-------empty--strings---", "-"));
+console.log(splitter('gotmultiplecharsforya', 'multiplecharsfor'));
+console.log(splitter('defaults to spaces for invalid seperator', null));
