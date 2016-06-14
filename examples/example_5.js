5062421c2dfd906cccda5563dbdfff6afee7a368
OoogleBoogle <simattfield@gmail.com>
2016-06-14 16:23:18 -0700

    corrected spelling

--- examples/example_5.js
+++ examples/example_5.js
@@ -3,21 +3,21 @@ function blankCheck(word, start, end, splits) {
   if (str) splits.push(str);
 }
 
-function splitter(word, seperator, start = 0, end = 0, splits = []) {
-  if (!seperator) {
-    seperator = ' ';
+function splitter(word, separator, start = 0, end = 0, splits = []) {
+  if (!separator) {
+    separator = ' ';
   }
   if (end === word.length ) {
     blankCheck(word, start, end, splits);
     return splits;
   }
-  if (word.substr(end, seperator.length) === seperator) {
+  if (word.substr(end, separator.length) === separator) {
     blankCheck(word, start, end, splits);
-    start = end + seperator.length
+    start = end + separator.length
   }
-  return splitter(word, seperator, start, end + 1, splits)
+  return splitter(word, separator, start, end + 1, splits)
 }
 
 console.log(splitter("Split---without-------empty--strings---", "-"));
 console.log(splitter('gotmultiplecharsforya', 'multiplecharsfor'));
-console.log(splitter('defaults to spaces for invalid seperator', null));
+console.log(splitter('defaults to spaces for invalid separator', null));
